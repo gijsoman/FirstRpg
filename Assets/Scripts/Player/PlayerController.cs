2215081817f491315c3da9616a34439e80c08776
gijsoman <gijsbakker1@hotmail.com>
2018-05-21 13:39:42 +0200

    Very basic player movement. Added a playercontroller and a player motor.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask MovementMask;
 
-	// Use this for initialization
-	void Start () {
-		
+    private Camera cam;
+    private PlayerMotor motor;
+
+	private void Start ()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, MovementMask))
+            {
+                motor.MoveToPoint(hit.point);
+                //Stop focussing any objects.
+            }
+        }
 	}
 }
