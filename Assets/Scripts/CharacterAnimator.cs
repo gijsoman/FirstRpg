55ec76a505ad56a4c49c0e7a6d6f56eb8fab78ef
gijsoman <gijsbakker1@hotmail.com>
2018-05-22 16:43:28 +0200

    Made a different folder for characters to keep things organized. Made a character controller and replaced the old GFX with the new player and it's animations.

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    public float LocomotionAnimationSmoothTime = .1f;
+
+    private Animator animator;
+    private NavMeshAgent agent;
+
+	private void Start ()
+    {
+        animator = GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	private void Update ()
+    {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, LocomotionAnimationSmoothTime, Time.deltaTime);
+	}
+}
