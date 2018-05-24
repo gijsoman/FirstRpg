7b5c43aa3782693a26b53a67f832fc899905e755
gijsoman <gijsbakker1@hotmail.com>
2018-05-24 14:10:20 +0200

    Created an inventory script and a GameManager object. Added singleton to the inventory script so only one instance can exist at a time.

---
+++ Assets/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    #region Singleton
+    public static Inventory Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogWarning("More than one instance of Inventory found!");
+            return;
+        }
+        Instance = this;
+    }
+    #endregion
+
+    public List<Item> Items = new List<Item>();
+
+    public void Add(Item item)
+    {
+        if (!item.IsDefaultItem)
+        {
+            Items.Add(item);
+        }
+    }
+
+    public void Remove(Item item)
+    {
+        Items.Remove(item);
+    }
+}
